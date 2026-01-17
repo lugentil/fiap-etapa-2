79cf64d5321823ebf08603b03ce653297d706f4e
Lucas <lugentil01@gmail.com>
2026-01-17 15:40:27 -0300

    kubernetes atualizado, faltando hpa

---
+++ kubernetes/analytics-service/aws-credential.sh
@@ -0,0 +1,4 @@
+kubectl create secret generic aws-credentials \
+  --from-literal=AWS_ACCESS_KEY_ID="$(aws configure get aws_access_key_id)" \
+  --from-literal=AWS_SECRET_ACCESS_KEY="$(aws configure get aws_secret_access_key)" \
+  --from-literal=AWS_SESSION_TOKEN="$(aws configure get aws_session_token)" -n analytics-service
